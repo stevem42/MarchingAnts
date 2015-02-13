083e38fa95e733f2cc6700871b3bc764533785d0
Steve Mckinnon <elevated42@yahoo.com.au>
2015-02-13 22:28:04 +1000

    second commit

--- app/src/main/java/steve/marchingants/MainActivity.java
+++ app/src/main/java/steve/marchingants/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
 String[] numbers = {"one","two","three","four","five","six","seven","eight","nine","ten"};
 
         for(String number : numbers){
-            String song = String.format("Ants go marching %1$s by %1$s. Hoorah! Hoorah!", number);
+            String song = String.format(getResources().getString(R.string.song_format), number);
             Log.d(TAG, song);
         }
 
